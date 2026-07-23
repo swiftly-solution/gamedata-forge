48807756a8aa790d958fd8e2e13959c0fc96e69d
skuzzis <andreieurt@gmail.com>
2026-07-23 21:47:02 +0300

    feat(Tier0): CommandLine

---
+++ GameData.Tier0/src/Core/CommandLine/CommandLine.cs
@@ -0,0 +1,82 @@
+using GameData.Tier0.Shared.CommandLine;
+
+namespace GameData.Tier0.Core.CommandLine;
+
+internal class CCommandLine : ICommandLine
+{
+    private string _commandLine = "";
+    private List<string> _parameters = [];
+    private Dictionary<string, string> _parameterValues = [];
+
+    public string GetCommandLineString()
+    {
+        return _commandLine;
+    }
+
+    public ulong GetParameterCount()
+    {
+        return (ulong)_parameters.Count;
+    }
+
+    public List<string> GetParameters()
+    {
+        return _parameters;
+    }
+
+    public string GetParameterValue(string parameter, string defaultValue = "")
+    {
+        if (!_parameterValues.TryGetValue(parameter, out var value))
+        {
+            return defaultValue;
+        }
+        return value;
+    }
+
+    public bool HasParameter(string parameter)
+    {
+        return _parameterValues.ContainsKey(parameter);
+    }
+
+    public void Initialize(string commandLine)
+    {
+        _commandLine = commandLine;
+        _parameters = [.. commandLine.Split(' ')];
+        _parameterValues = [];
+        for (int i = 0; i < _parameters.Count; i++)
+        {
+            if (_parameters[i].StartsWith('-'))
+            {
+                var key = _parameters[i][1..];
+                if (i + 1 < _parameters.Count && !_parameters[i + 1].StartsWith('-'))
+                {
+                    _parameterValues[key] = _parameters[i + 1];
+                    i++;
+                }
+                else
+                {
+                    _parameterValues[key] = "";
+                }
+            }
+        }
+    }
+
+    public void Initialize(string[] args)
+    {
+        Initialize(string.Join(' ', args));
+    }
+
+    public void InitializeWithAppName(string commandLine)
+    {
+        var args = commandLine.Split(' ');
+        if (args.Length > 0)
+        {
+            var appName = args[0];
+            var remainingArgs = args[1..];
+            Initialize(string.Join(' ', remainingArgs));
+        }
+        else
+        {
+            Initialize(commandLine);
+        }
+    }
+}
---
+++ GameData.Tier0/src/Shared/CommandLine/ICommandLine.cs
@@ -0,0 +1,15 @@
+namespace GameData.Tier0.Shared.CommandLine;
+
+public interface ICommandLine
+{
+    public void Initialize(string commandLine);
+    public void Initialize(string[] args);
+    public void InitializeWithAppName(string commandLine);
+
+    public bool HasParameter(string parameter);
+    public string GetParameterValue(string parameter, string defaultValue = "");
+
+    public ulong GetParameterCount();
+    public List<string> GetParameters();
+    public string GetCommandLineString();
+}
